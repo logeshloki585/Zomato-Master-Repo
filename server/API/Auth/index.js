95f708d41a365daa522d2e3d8ca584deeb9915be
logeshloki585 <logeshtv21@gmail.com>
2021-07-30 23:30:32 +0530

    google auth

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 
 // Models
 import { UserModel } from "../../database/user";
+import passport from "passport";
 
 const Router = express.Router();
 
@@ -45,9 +46,46 @@ Router.post("/signin", async (req,res) =>{
         const token = user.generateJwtToken();
         // return
         return res.status(200).json({token ,status: "succes"})
+
     }catch(error) {
         return res.status(500).json({ error: error.message});
     }
 });
+
+/*
+Route    /google
+Des      google signin
+params   none
+Access   Public
+Method   GET
+ */
+Router.get(
+    "/google",
+    passport.authenticate("google", {
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+      ],
+    })
+  )
+
+
+/*
+Route    /google/callback
+Des      google signin callback
+params   none
+Access   Public
+Method   GET
+ */
+
+Router.get(
+    "/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+      return res.json({ token: req.session.passport.user.token });
+    }
+  );
+
+
 
 export default Router;
