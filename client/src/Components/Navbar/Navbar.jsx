76909cc490ff44a527c544e4a44af7f60d38c96f
logeshloki585 <logeshtv21@gmail.com>
2021-08-19 16:12:47 +0530

    na

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import {IoMdArrowDropdown} from "react-icons/io";
 import {RiSearch2Line} from "react-icons/ri";
 
 const MobileNav =() =>{
-    return( <div className="flex w-full items-center justify-between md:hidden">
+    return( <div className="flex w-full items-center justify-between lg:hidden">
         <div className="w-28 ">
             <img className="w-full height-full" src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" />
         </div>
@@ -17,13 +17,14 @@ const MobileNav =() =>{
     );
 };
 
-const MediumNav = () => {
+const LargeNav = () => {
  return  <>
-    <div className="hidden gap-4  w-full items-center justify-between  md:flex lg:w-3/4 " >
+ <div className="container px-20 mx-auto">
+    <div className="hidden gap-4  w-full items-center justify-between  lg:flex  " >
         <div className="w-28 ">
             <img className="w-full height-full" src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" />
         </div>
-        <div className="w-full flex bg white shadow-md p-3 items-center border gap-3 border-gray-200 rounded">
+        <div className="w-3/4 flex bg white shadow-md p-3 items-center border gap-3 border-gray-200 rounded">
             <div className="flex  items-center gap-2 barder-r-2 border-gray-300 pr-2">
                 <span className="text-zomato-400">
                     <HiLocationMarker />
@@ -36,11 +37,12 @@ const MediumNav = () => {
                 <input type="text" className="w-full focus:outline-none" placeholder="Search for restaurant, cuisine or a dish" />
             </div>
         </div>
-        <div className="flex gap-2 ">
+        <div className="ml-20 gap-4 flex gap-2 ">
             <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
             <button className="text-gray-500 text-xl hover:text-gray-800">signup</button>
         </div>
     </div>
+  </div>
  </>
 }
 
@@ -52,7 +54,7 @@ const Navbar = () => {
     <>
     <nav className="p-4 flex bg-white shadow-md w-full items-center ">
             <MobileNav />
-            <MediumNav />
+            <LargeNav />
     </nav>
     </> 
     )
