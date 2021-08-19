7c9634ee530a78d726835f3e039712e405fe64a4
logeshloki585 <logeshtv21@gmail.com>
2021-08-19 16:03:04 +0530

    navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -19,26 +19,33 @@ const MobileNav =() =>{
 
 const MediumNav = () => {
  return  <>
-    <div className="hidden flex w-full items-center justify-between border border-gray-200 md:flex">
+    <div className="hidden gap-4  w-full items-center justify-between  md:flex lg:w-3/4 " >
         <div className="w-28 ">
             <img className="w-full height-full" src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" />
         </div>
-        <div className="w-full flex bg white shadow-md px-3 py-3 items-center border gap-3 border-gray-200 rounded">
+        <div className="w-full flex bg white shadow-md p-3 items-center border gap-3 border-gray-200 rounded">
             <div className="flex  items-center gap-2 barder-r-2 border-gray-300 pr-2">
                 <span className="text-zomato-400">
                     <HiLocationMarker />
                 </span>
-                <input type="text" placeholder="Bengaluru" />
+                <input type="text" className="w-full focus:outline-none" placeholder="Bengaluru" />
                 <IoMdArrowDropdown />
             </div>
-            <div className="flex items-center gap-2">
+            <div className="flex w-full items-center gap-2">
                 <RiSearch2Line />
-                <input type="text" placeholder="Search for restaurant, cuisine or a dish" />
+                <input type="text" className="w-full focus:outline-none" placeholder="Search for restaurant, cuisine or a dish" />
             </div>
+        </div>
+        <div className="flex gap-2 ">
+            <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
+            <button className="text-gray-500 text-xl hover:text-gray-800">signup</button>
         </div>
     </div>
  </>
 }
+
+
+
 
 const Navbar = () => {
     return(
